4e06fb0aefa3340d8dc64e453d88e35fface140f
kelvinuche10 <amkelvinuche@gmail.com>
2023-06-29 15:27:42 +0100

    third commit

--- dcrem/settings.py
+++ dcrem/settings.py
@@ -68,7 +68,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'elderco',
         'USER': 'root',
-        'PASSWORD':'password',
+        'PASSWORD':'10Kelvin!_',
         'HOST': 'localhost',
         'PORT': '3306', 
     }
